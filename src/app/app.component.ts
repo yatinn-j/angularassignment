3ef3f39fe600268e9082f70b00e6cb9e59d80533
Yatin Jain <yatin.jain@technossus.com>
2020-03-05 15:59:07 +0530

    diplay recipe details on click of each recipe item individually

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Angular Project';
+  loadedFeature: string = "recipe";
+
+  onNavigate = (feature: string) => {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,11 +9,15 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("This is a test recipe", "this is test recipe description", "https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg"),
-    new Recipe("This is a test recipe", "this is test recipe description", "https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg")
+    new Recipe("This is another test recipe", "this is another test recipe description", "https://static01.nyt.com/images/2013/06/26/dining/26JPFLEX1/26JPFLEX1-articleLarge-v3.jpg")
   ];
+  @Output() selectedItem = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+  recipeItemClicked = (recipe: Recipe) => {
+    this.selectedItem.emit(recipe);
   }
 
 }
