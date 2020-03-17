f64649f0bbbc6fd6551d3a441d1da7ed2cf63071
Yatin Jain <yatin.jain@technossus.com>
2020-03-17 23:15:50 +0530

    template driven angular form assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'My Angular Project';
+  title = "My Angular Project";
+  @ViewChild("f", { static: false }) form: NgForm;
+  submitted = false;
+  defaultSubscription = "advance";
+  result = {
+    email: "",
+    password: "",
+    subscription: ""
+  };
+
+  onFormSubmit = () => {
+    this.submitted = true;
+    this.result = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+      subscription: this.form.value.subscription
+    };
+
+    this.form.reset();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { DatabindingComponent } from './databinding/databinding.component';
-import { DirectivesExampleComponent } from './directives-example/directives-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    DatabindingComponent,
-    DirectivesExampleComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
